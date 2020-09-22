73975041f43ae4b829f14a0552eb1dbefb8e56c7
Alan.Lin <eric820209@gmail.com>
2020-09-23 01:40:18 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module learnmodule/alan/repo
+
+go 1.15
+
+require rsc.io/quote v1.5.2
---
+++ hello.go
@@ -0,0 +1,8 @@
+package hello
+
+import "rsc.io/quote"
+
+//Hello for test
+func Hello() string {
+	return quote.Hello()
+}
